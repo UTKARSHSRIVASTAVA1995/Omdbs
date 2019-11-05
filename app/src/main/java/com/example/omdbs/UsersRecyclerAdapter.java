d104defcadde09439ee913ceadb9699cbdffeb77
utkarsh.psrivastava <utkarshsrivastava1995@gmail.com>
2019-11-05 17:32:28 +0530

    registered activity recycler view

---
+++ app/src/main/java/com/example/omdbs/UsersRecyclerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.omdbs;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UsersRecyclerAdapter extends RecyclerView.Adapter<UsersRecyclerAdapter.UserViewHolder> {
+
+    private List<User> listUsers;
+
+    public UsersRecyclerAdapter(List<User> listUsers) {
+        this.listUsers = listUsers;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_user_recycler, parent, false);
+
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        holder.textViewName.setText(listUsers.get(position).getName());
+        holder.textViewEmail.setText(listUsers.get(position).getEmail());
+        holder.textViewPassword.setText(listUsers.get(position).getPassword());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.v(UsersRecyclerAdapter.class.getSimpleName(), "" + listUsers.size());
+        return listUsers.size();
+    }
+
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+        public AppCompatTextView textViewEmail;
+        public AppCompatTextView textViewPassword;
+
+        public UserViewHolder(View view) {
+            super(view);
+            textViewName = view.findViewById(R.id.textViewName);
+            textViewEmail = view.findViewById(R.id.textViewEmail);
+            textViewPassword = view.findViewById(R.id.textViewPassword);
+        }
+    }
+}
+
